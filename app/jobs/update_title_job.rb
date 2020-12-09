7a908d87ae10df139ea6c4d0fbce666cf6b4a788
christphrd <diep.christopher@gmail.com>
2020-12-09 18:46:47 -0500

    UpdateTitleJob class perform methods updates title

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,15 @@
+require 'open-uri'
+
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
-
+    @short_url = ShortUrl.find(short_url_id)
+    title = ''
+    URI.open(@short_url.full_url) do |f|
+      str = f.read
+      title = str.scan(/<title>(.*?)<\/title>/)[0][0]
+    end
+    @short_url.update(title: title)
   end
 end
